aea99c9a6bfd71d0b0187e4fe247d6f3678d8823
Sarah Iensen Morais <138230433+sarahiensen@users.noreply.github.com>
2024-11-11 11:31:47 -0300

    Navicon update

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function toggleMenu() {
-    const menu = document.querySelector(".menu-links");    
+    const menu = document.querySelector(".menu-links");
     const icon = document.querySelector(".hamburger-icon");
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
 
-//sair do hamburger menu com esc
+// Fechar o menu com esc
 document.addEventListener("keydown", (event) => {
     const menu = document.querySelector(".menu-links");
     const icon = document.querySelector(".hamburger-icon");
@@ -14,4 +14,15 @@ document.addEventListener("keydown", (event) => {
         icon.classList.remove("open");
     }
 });
+
+// Fechar o menu clicando fora
+document.addEventListener("click", (event) => {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    if (!menu.contains(event.target) && !icon.contains(event.target) && menu.classList.contains("open")) {
+        menu.classList.remove("open");
+        icon.classList.remove("open");
+    }
+});
+
 
